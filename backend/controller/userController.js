a289fd05049bb184c516b5b42ee4f9f3690fb570
nitinpatil77 <patilnitin1437@gmail.com>
2024-12-28 23:47:16 +0530

    user authentication done

---
+++ backend/controller/userController.js
@@ -0,0 +1,76 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// create token
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+// user login route
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      return res.json({ success: true, token });
+    } else {
+      return res.json({ success: true, message: "Invalid credentails" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// user register route
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //  checking user is allready exists or not
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User allready exits" });
+    }
+
+    // validate email and strong password;
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({ success: true, token }); // pass token into response
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// admin login route
+export const adminLogin = async (req, res) => {};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloundinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 // App Config
 const app = express();
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());  // To parse JSON request bodies
 
 // API Endpoint
+app.use('/api/user',userRouter)
+
 app.get('/', (req, res) => {
     res.send('API Working');
 });

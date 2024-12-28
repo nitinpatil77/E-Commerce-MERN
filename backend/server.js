b86fc777deaf423190982b8ce76d71fa49498a40
nitinpatil77 <patilnitin1437@gmail.com>
2024-12-28 18:21:34 +0530

    done mongoDB connection as well as cloudinary connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloundinary from './config/cloudinary.js';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 3535;
+connectDB();
+connectCloundinary();
+
+// Middleware
+app.use(cors());
+app.use(express.json());  // To parse JSON request bodies
+
+// API Endpoint
+app.get('/', (req, res) => {
+    res.send('API Working');
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log(`Server is started on PORT: ${port}`);
+});

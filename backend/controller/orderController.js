73322216b6da35d37e51081b56e194decdf04a90
nitinpatil77 <patilnitin1437@gmail.com>
2025-01-01 16:03:00 +0530

    done order display and update feature via backend api to admin

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -31,7 +31,15 @@ const placeOrderStripe = async (req, res) => {};
 const placeOrderRazorpay = async (req, res) => {};
 
 // All orders data for Admin
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // User order data for Frontend
 const userOrders = async (req, res) => {
@@ -47,7 +55,18 @@ const userOrders = async (req, res) => {
 };
 
 // update status from admin pannel
-const updateStatus = async (req, res) => {};
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body; // Ensure orderId matches the frontend
+    
+    await orderModel.findByIdAndUpdate(orderId, { status });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 
 export {
   placeOrder,
